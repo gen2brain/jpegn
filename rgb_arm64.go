2cbafadc7a5df327bba0db267643fd532e6c203e
Milan Nikolic <gen2brain@gmail.com>
2026-08-06 18:54:59 +0200

    Add NEON RGB to YCbCr conversion

--- rgb_arm64.go
+++ rgb_arm64.go
@@ -2,7 +2,18 @@
 
 package jpegn
 
+//go:noescape
+func rgbToYCbCrNEON(dstY, dstCb, dstCr, src []byte, n int)
+
 // rgbToYCbCrRow converts n pixels with a four byte stride to YCbCr planes.
 func rgbToYCbCrRow(dstY, dstCb, dstCr, src []byte, n int) {
+	if v := n &^ 15; v > 0 {
+		rgbToYCbCrNEON(dstY, dstCb, dstCr, src, v)
+
+		dstY, dstCb, dstCr = dstY[v:], dstCb[v:], dstCr[v:]
+		src = src[v*4:]
+		n -= v
+	}
+
 	rgbToYCbCrRowScalar(dstY, dstCb, dstCr, src, n)
 }
